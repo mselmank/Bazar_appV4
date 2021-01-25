ba41e6c5c5a187da6d52a46949344d295d8a683f
Jaime Perez <jaime.perez.varas@gmail.com>
2021-01-25 00:50:59 -0300

    Avance de 24 de Enero Ruta de Ordenes!

--- app/src/main/java/cl/matiasselman_android/bazar_appv4/ui/orderList/ListOrderRecyclerViewAdapter.java
+++ app/src/main/java/cl/matiasselman_android/bazar_appv4/ui/orderList/ListOrderRecyclerViewAdapter.java
@@ -12,9 +12,19 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.firestore.GeoPoint;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.Duration;
+import com.google.maps.model.TravelMode;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 import cl.matiasselman_android.bazar_appv4.R;
 import cl.matiasselman_android.bazar_appv4.models.Order;
@@ -35,7 +45,7 @@ public class ListOrderRecyclerViewAdapter extends RecyclerView.Adapter<ListOrder
     @Override
     public OrderListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_order_list_item, parent, false);
+            .inflate(R.layout.fragment_order_list_item, parent, false);
         context = parent.getContext();
         return new OrderListViewHolder(view);
     }
@@ -54,7 +64,44 @@ public class ListOrderRecyclerViewAdapter extends RecyclerView.Adapter<ListOrder
         String urlAddress = "http://maps.google.com/maps?q=" + geoPoint.getLatitude() + "," + geoPoint.getLongitude() + "(" + mMarkerName + ")&iwloc=A&hl=es";
         String uri = String.format(Locale.ENGLISH, "geo:%f,%f?q=" + mMarkerName, geoPoint.getLatitude(), geoPoint.getLongitude());
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlAddress));
+        try {
+            String hola = getDurationForRoute("-33.4371288,-70.6474297", geoPoint.getLatitude() + "," + geoPoint.getLongitude());
+        }catch (Exception error){
+            error.printStackTrace();
+        }
         context.startActivity(intent);
+    }
+
+    /*
+     Use Google's directions api to calculate the estimated time needed to
+     drive from origin to destination by car.
+
+     @param origin The address/coordinates of the origin (see {@link DirectionsApiRequest#origin(String)} for more information on how to format the input)
+     @param destination The address/coordinates of the destination (see {@link DirectionsApiRequest#destination(String)} for more information on how to format the input)
+
+     @return The estimated time needed to travel human-friendly formatted
+     */
+    public String getDurationForRoute(String origin, String destination) throws InterruptedException, ApiException, IOException {
+        String API_KEY = "AIzaSyBc1R82oRWGpn-woMgkxL8j8WQIzS2tIGw";
+        // - We need a context to access the API
+        GeoApiContext geoApiContext = new GeoApiContext.Builder()
+            .apiKey(API_KEY)
+            .build();
+
+        // - Perform the actual request
+        DirectionsResult directionsResult = DirectionsApi.newRequest(geoApiContext)
+            .mode(TravelMode.DRIVING)
+            .origin(origin)
+            .waypoints(destination)
+            .optimizeWaypoints(true)
+            .destination(origin)
+            .await();
+
+        // - Parse the result
+        DirectionsRoute route = directionsResult.routes[0];
+        DirectionsLeg leg = route.legs[0];
+        Duration duration = leg.duration;
+        return duration.humanReadable;
     }
 
     @Override
